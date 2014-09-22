6780c816b704fe3d8daa68057e263bab1001c0e2
Eric <Ericrius1@gmail.com>
2014-09-22 13:39:10 -0700

    correctly routing between worlds

--- public/js/hub.js
+++ public/js/hub.js
@@ -10,36 +10,35 @@ hub.load = function() {
 
 hub.controller = function() {
   this.worlds = hub.load()
-  this.worlds.then(function(worlds){
-    worlds.map(function(world){
-    })
-  });
-  
 }
 
 hub.view = function(ctrl) {
   return m(".worlds", [
     ctrl.worlds().map(function(world, index) {
+
+      var element = "a[href='/world/" + world._id + "']";
       return m('div', [
-        m("a[href] = '/world/]", {config: m.route}, 'world ' + index)
+        m(element, {config: m.route}, 'world ' + index)
       ])
     })
   ]);
 };
 
 world.controller = function(){
+  this.id = m.route.param('worldID');
 
 }
 
-world.view = function(){
-  return m('div');
+world.view = function(ctrl){
+  return m('div', ctrl.id);
 }
 
 m.route.mode = "hash"
 m.route(document.body, '/', {
   "/": hub,
-  "/world" : world
+  "/world/:worldID": world
 });
+
 
 
 
