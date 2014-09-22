b794c2f7b93c6d6da38c2da10ff0dc561959cf49
Eric <Ericrius1@gmail.com>
2014-09-21 19:02:54 -0700

    init

---
+++ public/js/main.js
@@ -0,0 +1,59 @@
+var todo = {};
+
+var todo = {};
+
+todo.Todo = function(data) {
+  this.description = m.prop(data.description);
+  this.done = m.prop(false);
+};
+
+//the TodoList class is a list of Todo's
+todo.TodoList = Array;
+
+
+//the controller uses three model-level entities, of which one is a custom defined class:
+//'Todo' is the central class in this application
+//'list' is merely a generic array, with standard array methods
+//'description' is a temporary storage box that holds a string
+
+//the 'add' method simply adds a new todo to the list
+todo.controller = function() {
+  this.list = new todo.TodoList();
+  this.description = m.prop("");
+
+  this.add = function() {
+    if (this.description()) {
+      this.list.push(new todo.Todo({
+        description: this.description()
+      }));
+      this.description("");
+    }
+  }.bind(this);
+}
+
+todo.view = function(ctrl) {
+  return ("html", [
+    m("body", [
+      m("input", {
+        onchange: m.withAttr('value', ctrl.description),
+        value: ctrl.description()
+      }),
+      m("button", {
+        onclick: ctrl.add
+      }, "Add"),
+      m("table", [
+        ctrl.list.map(function(task, index) {
+          return m("tr", [
+            m("td", [
+              m('input[type=checkbox]',{onclick: m.withAttr("checked", task.done), checked: task.done()})
+            ]),
+            m("td", { style: {textDecoration: task.done() ? "line-through": "none"}}, task.description()),
+          ])
+        })
+      ])
+    ])
+  ]);
+};
+
+//inititalize the application
+m.module(document, todo);
---
+++ server.js
@@ -0,0 +1,12 @@
+//modules
+
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 8080
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(port);
+console.log('Magic happens on port' + port);
