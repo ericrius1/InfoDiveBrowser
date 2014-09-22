448dafa71e65233469a2115f4c016d3ee4b4f784
Eric <Ericrius1@gmail.com>
2014-09-21 20:09:08 -0700

    added mongo db to be able to store serialized playgrounds

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 //modules
+// mongodb://heroku_app29862006:72o7v0fsmoaro2ra30grr8n57n@ds039000.mongolab.com:39000/heroku_app29862006
 
 var express = require('express');
 var app = express();
-
+var mongodb = require('mongodb');
+var mongojs = require('mongojs');
 var port = process.env.PORT || 8080
-
 app.use(express.static(__dirname + '/public'));
+
+var uri = 'mongodb://heroku_app29862006:72o7v0fsmoaro2ra30grr8n57n@ds039000.mongolab.com:39000/heroku_app29862006';
+// var uri = 'infodive';
+var seedData = [{
+  vertex: {
+    x: 0,
+    y: 1,
+    z: 0
+  }
+}]
+var db = mongojs.connect(uri);
+db.createCollection('worlds');
+var worlds = db.collection('worlds');
+worlds.insert(seedData[0], function(err, result) {
+  if (err) throw err;
+});
+
+
+
+
 
 
 app.listen(port);
