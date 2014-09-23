d2bb5136dcabf4187fa5b2a9c14edb52be50b6f9
Eric <Ericrius1@gmail.com>
2014-09-22 19:03:09 -0700

    pointer controls

---
+++ public/js/controls.js
@@ -0,0 +1,135 @@
+var Controls = function() {
+  // activePrimitive = G.primitives['49'];
+  var timeoutId;
+  var firstTime = true;
+  G.fpsControls = new THREE.PointerLockControls(G.camera);
+  G.controlObject = G.fpsControls.getObject();
+
+  G.scene.add(G.fpsControls.getObject());
+  var mouseTimeoutId;
+  var pLockEnabled = false;
+
+  function teleport(point) {
+    G.controlObject.position.set(point);
+  }
+
+  var havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
+
+  if (havePointerLock) {
+
+    var element = document.body;
+
+    var pointerlockchange = function(event) {
+      if (document.pointerLockElement === element || document.mozPointerLockElement === element || document.webkitPointerLockElement === element) {
+
+        G.fpsControls.enabled = true;
+        if(!pLockEnabled){
+          // $('#cursor').addClass('active')
+          // $(document).on('mousedown', mouseDown);
+          // $(document).on('mouseup', mouseRelease);
+
+          // $(document).on('keydown', keyPressed);
+          pLockEnabled = true
+        }
+
+
+
+
+      } else {
+        pLockEnabled = false;
+        G.fpsControls.enabled = false;
+        // $('#cursor').removeClass('active')
+        // $(document).off('mousedown');
+        // $(document).off('mouseup');
+
+        // $(document).off('keydown');
+
+
+      }
+
+    };
+
+    var pointerlockerror = function(event) {
+
+
+    };
+    // Hook pointer lock state change events
+    // document.addEventListener('pointerlockchange', pointerlockchange, false);
+    document.addEventListener('mozpointerlockchange', pointerlockchange, false);
+    document.addEventListener('webkitpointerlockchange', pointerlockchange, false);
+
+    document.addEventListener('pointerlockerror', pointerlockerror, false);
+    document.addEventListener('mozpointerlockerror', pointerlockerror, false);
+    document.addEventListener('webkitpointerlockerror', pointerlockerror, false);
+
+    document.addEventListener('click', function(event) {
+
+
+
+      // Ask the browser to lock the pointer
+      element.requestPointerLock = element.requestPointerLock || element.mozRequestPointerLock || element.webkitRequestPointerLock;
+
+      if (/Firefox/i.test(navigator.userAgent)) {
+
+        var fullscreenchange = function(event) {
+
+          if (document.fullscreenElement === element || document.mozFullscreenElement === element || document.mozFullScreenElement === element) {
+
+            document.removeEventListener('fullscreenchange', fullscreenchange);
+            document.removeEventListener('mozfullscreenchange', fullscreenchange);
+
+            element.requestPointerLock();
+          }
+
+        }
+
+        document.addEventListener('fullscreenchange', fullscreenchange, false);
+        document.addEventListener('mozfullscreenchange', fullscreenchange, false);
+
+        element.requestFullscreen = element.requestFullscreen || element.mozRequestFullscreen || element.mozRequestFullScreen || element.webkitRequestFullscreen;
+
+        element.requestFullscreen();
+
+      } else {
+        element.requestPointerLock();
+
+      }
+
+    }, false);
+
+  }
+
+  this.update = function() {
+    G.fpsControls.update();
+  }
+
+
+  // function mouseDown() {
+  //   activePrimitive.spawn();
+  //   $('#cursor').addClass('held');
+  //   timeoutId = setInterval(function(){
+  //     activePrimitive.spawn();
+  //   }, activePrimitive._spawnInterval);
+
+  // }
+
+  // function mouseRelease() {
+  //   $('#cursor').removeClass('held');
+  //   window.clearInterval(timeoutId)
+  //   activePrimitive.unspawn();
+  // }
+
+  // function keyPressed(event) {
+  //   if (event.keyCode in G.primitives) {
+  //     _.each(G.primitives, function(primitive){
+  //       primitive.constructor.$menuItem.removeClass('active')
+  //     })
+
+  //     activePrimitive = G.primitives[event.keyCode];
+  //     activePrimitive.constructor.$menuItem.addClass('active');
+
+  //    }
+  // };
+
+
+}
--- public/js/world.js
+++ public/js/world.js
@@ -1,27 +1,59 @@
-
-
-G.initialize = function(){
+G.initialize = function() {
   G.w = window.innerWidth;
   G.h = window.innerHeight;
   G.glContainer = document.getElementById('glContainer');
   G.scene = new THREE.Scene();
-  G.camera = new THREE.PerspectiveCamera(45, this.w/this.h, 1, 1000)
-  G.camera.position.z = 50;
+  G.camera = new THREE.PerspectiveCamera(45, this.w / this.h, 1, 20000)
+  // G.camera.position.z = 50
   G.renderer = new THREE.WebGLRenderer()
   G.renderer.setSize(G.w, G.h);
-  G.glContainer.appendChild(G.renderer.domElement)
+  G.glContainer.appendChild(G.renderer.domElement);
+  var sphere = new THREE.Mesh(new THREE.SphereGeometry(5, 16));
+  G.scene.add(sphere);
+
+  // sphere.scale.multiplyScalar(5 * data);
+
+  // G.controls = new THREE.OrbitControls(G.camera, G.renderer.domElement);
+
+  //GROUND
+  var groundGeo = new THREE.PlaneGeometry(10000, 10000, 32, 32)
+  var groundMat = new THREE.MeshBasicMaterial({
+    color: 0x0000ff,
+    transparent: true,
+    opacity: 0.1,
+    wireframe: true
+  })
+  var ground = new THREE.Mesh(groundGeo, groundMat);
+  ground.rotation.x = -Math.PI / 2
+  G.scene.add(ground)
+
+  G.renderer.autoClear = false;
+  G.renderModel = new THREE.RenderPass(G.scene, G.camera);
+  G.effectBloom = new THREE.BloomPass(1.1);
+  G.effectCopy = new THREE.ShaderPass(THREE.CopyShader);
+  G.effectFXAA = new THREE.ShaderPass(THREE.FXAAShader);
+  G.effectFXAA.uniforms['resolution'].value.set(1 / G.w, 1 / G.h);
+  G.effectCopy.renderToScreen = true;
+
+  G.composer = new THREE.EffectComposer(G.renderer)
+  G.composer.addPass(G.renderModel);
+  G.composer.addPass(G.effectFXAA);
+  G.composer.addPass(G.effectBloom);
+  G.composer.addPass(G.effectCopy);
 
 }
 
 G.initialize();
-animate()
 
-function animate(){
+function animate() {
+  G.renderer.clear()
+  G.composer.render();
+  G.controls.update();
+
   window.requestAnimationFrame(animate);
-  G.renderer.render(G.scene, G.camera);
 }
 
-G.onResize = function(){
+G.onResize = function() {
   this.w = window.innerWidth;
   this.h = window.innerHeight;
   this.ratio = this.w / this.h;
@@ -30,13 +62,20 @@ G.onResize = function(){
   this.camera.updateProjectionMatrix();
   this.renderer.setSize(this.w, this.h);
 
+  G.effectFXAA.uniforms['resolution'].value.set(1 / this.w, 1 / this.h);
+
+  G.composer.reset();
+
 }
 
-G.createScene = function(data){
-  console.log("DATA", data)
-  var sphere = new THREE.Mesh(new THREE.SphereGeometry(10, 16));
+G.createScene = function(data) {
+  console.log("DATA", data);
+  var sphere = new THREE.Mesh(new THREE.SphereGeometry(5, 16));
   G.scene.add(sphere);
-  sphere.scale.multiplyScalar(10 * data);
+  sphere.position.z = -100
+  sphere.scale.multiplyScalar(5 * data);
+  G.controls = new Controls(G.camera);
+  animate()
 }
 
 window.addEventListener('resize', G.onResize.bind(G), false);
